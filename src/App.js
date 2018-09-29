129b70f1f9a980fc9d3d53517c19bf09ad309b4f
Daniele <daniele.rb6@gmail.com>
2018-09-29 18:24:38 +0200

    chore: Install npm, ESLint, Prettier and Husky
    Added tools like npm, ESLint, Prettier and Husky (to automatically format our JavaScript commits) with the needed configuration files.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
+/* global React ReactDOM */
+
 const Article = () => {
-  return React.createElement("div", {className: "content"}, [
+  return React.createElement("div", { className: "content" }, [
     React.createElement("h1", {}, "Welcome!"),
-    React.createElement("p", {}, "This app is an exercise to demonstrate how to add authentication to a React app.")
+    React.createElement(
+      "p",
+      {},
+      "This app is an exercise to demonstrate how to add authentication to a React app."
+    )
   ]);
 };
 
 const App = () => {
-  return React.createElement(
-    "div", {className: "container"},
-    [
-      React.createElement("div", {className: "header"}, "React User Authentication"),
-      React.createElement(Article)
-    ]    
-  );
+  return React.createElement("div", { className: "container" }, [
+    React.createElement(
+      "div",
+      { className: "header" },
+      "React User Authentication"
+    ),
+    React.createElement(Article)
+  ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
