7ad21a6d529494215aaef17b35e5453c44236821
Daniele <daniele.rb6@gmail.com>
2018-12-18 17:39:03 +0100

    feat: Add login and logout to navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
+import { Link } from "@reach/router";
+import { Navbar, NavbarBrand, Nav, NavItem } from "reactstrap";
+import authService from "../utils/auth-service";
 
-const Header = () => <div className="header">React User Authentication</div>;
+class Header extends Component {
+  logout = () => {
+    authService().logout();
+  };
+
+  render() {
+    return (
+      <div className="header">
+        <Navbar color="light" light>
+          <NavbarBrand>React User Authentication</NavbarBrand>
+          <Nav>
+            <NavItem>
+              <Link className="link-reach-router navbar-link-text" to="/login">
+                Log In
+              </Link>
+            </NavItem>
+            <NavItem>
+              <Link
+                className="link-reach-router navbar-link-text"
+                to="/"
+                onClick={this.logout}
+              >
+                Log Out
+              </Link>
+            </NavItem>
+          </Nav>
+        </Navbar>
+      </div>
+    );
+  }
+}
 
 export default Header;
