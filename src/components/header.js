93315eb5e34193622bc08375facc30f902d4217b
Daniele <daniele.rb6@gmail.com>
2018-12-28 12:55:39 +0100

    refactor: Let Header render children

--- src/components/header.js
+++ src/components/header.js
@@ -1,50 +1,27 @@
 import React, { Component } from "react";
 import { Link } from "@reach/router";
 import { Navbar, NavbarBrand, Nav, NavItem } from "reactstrap";
-import authService from "../utils/auth-service";
 
 class Header extends Component {
-  logout = () => {
-    authService().logout();
-  };
-
   render() {
     return (
       <div className="header">
         <Navbar color="light" light>
           <NavbarBrand>React User Authentication</NavbarBrand>
           <Nav>
-            {!authService().isAuthenticated() && (
-              <NavItem>
-                <Link
-                  className="link-reach-router navbar-link-text"
-                  to="/login"
-                >
-                  Log In
-                </Link>
-              </NavItem>
-            )}
-            {authService().isAuthenticated() && (
-              <>
-                <NavItem>
+            {this.props.links
+              .filter(link => link.authentication === this.props.authenticated)
+              .map((link, index) => (
+                <NavItem key={index}>
                   <Link
                     className="link-reach-router navbar-link-text"
-                    to="/profile"
+                    to={link.navigationLink}
+                    onClick={link.onClickProp}
                   >
-                    Profile
+                    {`${link.text}`}
                   </Link>
                 </NavItem>
-                <NavItem>
-                  <Link
-                    className="link-reach-router navbar-link-text"
-                    to="/"
-                    onClick={this.logout}
-                  >
-                    Log Out
-                  </Link>
-                </NavItem>
-              </>
-            )}
+              ))}
           </Nav>
         </Navbar>
       </div>
--- src/components/home.js
+++ src/components/home.js
@@ -1,14 +1,50 @@
-import React from "react";
+import React, { Component } from "react";
 import Article from "./article";
 import Header from "./header";
+import authService from "../utils/auth-service";
 
-const Home = () => {
-  return (
-    <div className="container">
-      <Header />
-      <Article />
-    </div>
-  );
-};
+class Home extends Component {
+  state = {
+    authenticated: false,
+    links: [
+      {
+        text: "Log In",
+        navigationLink: "/login",
+        authentication: false,
+        onClickProp: undefined
+      },
+      {
+        text: "Profile",
+        navigationLink: "/profile",
+        authentication: true,
+        onClickProp: undefined
+      },
+      {
+        text: "Log Out",
+        navigationLink: "/",
+        authentication: true,
+        onClickProp: () => authService().logout()
+      }
+    ]
+  };
+
+  componentDidMount() {
+    this.setState({
+      authenticated: authService().isAuthenticated()
+    });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <Header
+          links={this.state.links}
+          authenticated={this.state.authenticated}
+        />
+        <Article />
+      </div>
+    );
+  }
+}
 
 export default Home;
