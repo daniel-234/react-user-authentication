f0e0d469a1dbf9eed0454668030ba9cf7e44e291
Daniele <daniele.rb6@gmail.com>
2018-12-29 11:11:54 +0100

    fix: Fix Home component state

--- src/components/header.js
+++ src/components/header.js
@@ -1,32 +1,30 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link } from "@reach/router";
 import { Navbar, NavbarBrand, Nav, NavItem } from "reactstrap";
 
-class Header extends Component {
-  render() {
-    return (
-      <div className="header">
-        <Navbar color="light" light>
-          <NavbarBrand>React User Authentication</NavbarBrand>
-          <Nav>
-            {this.props.links
-              .filter(link => link.authentication === this.props.authenticated)
-              .map((link, index) => (
-                <NavItem key={index}>
-                  <Link
-                    className="link-reach-router navbar-link-text"
-                    to={link.navigationLink}
-                    onClick={link.onClickProp}
-                  >
-                    {`${link.text}`}
-                  </Link>
-                </NavItem>
-              ))}
-          </Nav>
-        </Navbar>
-      </div>
-    );
-  }
+function Header(props) {
+  return (
+    <div className="header">
+      <Navbar color="light" light>
+        <NavbarBrand>React User Authentication</NavbarBrand>
+        <Nav>
+          {props.links
+            .filter(link => link.authentication === props.authenticated)
+            .map((link, index) => (
+              <NavItem key={index}>
+                <Link
+                  className="link-reach-router navbar-link-text"
+                  to={link.navigationLink}
+                  onClick={link.onClickProp}
+                >
+                  {link.text}
+                </Link>
+              </NavItem>
+            ))}
+        </Nav>
+      </Navbar>
+    </div>
+  );
 }
 
 export default Header;
--- src/components/home.js
+++ src/components/home.js
@@ -6,33 +6,49 @@ import authService from "../utils/auth-service";
 class Home extends Component {
   state = {
     authenticated: false,
-    links: [
-      {
-        text: "Log In",
-        navigationLink: "/login",
-        authentication: false,
-        onClickProp: undefined
-      },
-      {
-        text: "Profile",
-        navigationLink: "/profile",
-        authentication: true,
-        onClickProp: undefined
-      },
-      {
-        text: "Log Out",
-        navigationLink: "/",
-        authentication: true,
-        onClickProp: () => authService().logout()
-      }
-    ]
+    links: []
   };
 
   componentDidMount() {
+    /*
+     * Check if the user is authenticated (i.e. there is a valid token)
+     * and fill the array after the component has been mounted.
+     */
+    this.setState({
+      authenticated: authService().isAuthenticated(),
+      links: [
+        {
+          text: "Log In",
+          navigationLink: "/login",
+          authentication: false,
+          onClickProp: undefined
+        },
+        {
+          text: "Profile",
+          navigationLink: "/profile",
+          authentication: true,
+          onClickProp: undefined
+        },
+        {
+          text: "Log Out",
+          navigationLink: "/",
+          authentication: true,
+          onClickProp: this.logout
+        }
+      ]
+    });
+  }
+
+  /* 
+   * Log out the user, by removing the stored token, and 
+   * check if the user is authenticated. 
+   */
+  logout = () => {
+    authService().logout();
     this.setState({
       authenticated: authService().isAuthenticated()
     });
-  }
+  };
 
   render() {
     return (
