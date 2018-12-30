11c0525ac4cb52582351436d6a8f827f6036d548
Daniele <daniele.rb6@gmail.com>
2018-12-30 11:01:44 +0100

    refactor: Refactor Profile component
    Changed the component to be a class, with state.
    Added menu links which would show depending on the authentication state of the user.
    Added a message to be shown if trying to access the page without being authenticated, prompting the user to do that.

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,38 +1,95 @@
-import React from "react";
-import { Card, CardText, Navbar, NavbarBrand, Row, Col } from "reactstrap";
+import React, { Component } from "react";
+import { Card, CardText, Row, Col } from "reactstrap";
+import Header from "./header";
 import jwtDecode from "jwt-decode";
+import authService from "../utils/auth-service";
 
-const Profile = () => {
-  let token = localStorage.getItem("token");
-  let payload = jwtDecode(token);
+class Profile extends Component {
+  state = {
+    authenticated: false,
+    links: []
+  };
 
-  return (
-    <div className="container">
-      <div className="header">
-        <Navbar color="light" light>
-          <NavbarBrand>React User Authentication</NavbarBrand>
-        </Navbar>
+  componentDidMount() {
+    this.setState({
+      authenticated: authService().isAuthenticated(),
+      links: [
+        {
+          text: "Log In",
+          navigationLink: "/login",
+          authentication: false,
+          onClickProp: undefined
+        },
+        {
+          text: "Home",
+          navigationLink: "/",
+          authentication: true,
+          onClickProp: undefined
+        },
+        {
+          text: "Log Out",
+          navigationLink: "/",
+          authentication: true,
+          onClickProp: this.logout
+        }
+      ]
+    });
+  }
+
+  /* 
+   * Log out the user, by removing the stored token, and 
+   * check if the user is authenticated. 
+   */
+  logout = () => {
+    authService().logout();
+    this.setState({
+      authenticated: authService().isAuthenticated()
+    });
+  };
+
+  render() {
+    let token = localStorage.getItem("token");
+    let payload;
+    if (token) {
+      payload = jwtDecode(token);
+    }
+
+    return (
+      <div className="container">
+        <Header
+          links={this.state.links}
+          authenticated={this.state.authenticated}
+        />
+        {token ? (
+          <div className="content content-light-background">
+            <h2>Profile</h2>
+            <Row>
+              <Col sm="3">
+                <Card>
+                  <CardText>Profile image</CardText>
+                </Card>
+              </Col>
+              <Col sm="9">
+                <Card>
+                  <h3>{payload.username}</h3>
+                  <hr />
+                  <h4>Payload</h4>
+                  <pre>{JSON.stringify(payload, null, 2)}</pre>
+                </Card>
+              </Col>
+            </Row>
+          </div>
+        ) : (
+          <div className="content">
+            <p>{`You don't have permissions to access a profile page.`}</p>
+            <p>
+              {`Please, head to the Login page to log in or register as a user.`}
+            </p>
+          </div>
+        )}
       </div>
-      <div className="content content-light-background">
-        <h2>Profile</h2>
-        <Row>
-          <Col sm="3">
-            <Card>
-              <CardText>Profile image</CardText>
-            </Card>
-          </Col>
-          <Col sm="9">
-            <Card>
-              <h3>{payload.username}</h3>
-              <hr />
-              <h4>Payload</h4>
-              <pre>{JSON.stringify(payload, null, 2)}</pre>
-            </Card>
-          </Col>
-        </Row>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Profile;
